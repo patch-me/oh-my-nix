5f8dda9d41a127d34a7e6ef8a832c67bd4b94798
Brioche <thomasderudder@sfr.fr>
2024-07-16 15:23:07 +0200

    test sshfs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -119,6 +119,13 @@
   ventoy.enable = true;
   music.enable = true;
   android.enable = true;
+  sshfs.enable = true;
+  fileSystems."/mnt/mypatch" = {
+    device = "brioche@mypatch.fr:/home/brioche";
+    fsType = "sshfs";
+    options =
+      [ "nodev" "noatime" "allow_other" "IdentityFile=/root/.ssh/id_rsa" ];
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -130,21 +137,13 @@
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
 }
--- modules/nixos/sshfs/sshfs.nix
+++ modules/nixos/sshfs/sshfs.nix
@@ -2,12 +2,5 @@
 
   options = { sshfs.enable = lib.mkEnableOption "enables dwm"; };
 
-  config = lib.mkIf config.sshfs.enable {
-    fileSystems."/mnt/mypatch" = {
-      device = "brioche@mypatch.fr:/";
-      fsType = "sshfs";
-      options =
-        [ "nodev" "noatime" "allow_other" "IdentityFile=/root/.ssh/id_rsa" ];
-    };
-  };
+  config = lib.mkIf config.sshfs.enable { };
 }
