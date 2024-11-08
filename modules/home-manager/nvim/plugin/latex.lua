f70a6761c8a91f73a949c66797031052839515e7
ripitchip <thomasderudder@sfr.fr>
2024-11-08 09:37:56 +0100

    Wrong file type

--- modules/home-manager/nvim/plugin/latex.lua
+++ modules/home-manager/nvim/plugin/latex.lua
@@ -3,14 +3,13 @@ g.vimtex_view_method = "zathura"
 g.vimtex_view_method = "zathura_simple"
 g.vimtex_compiler_latexmk_engines = { ["_"] = "-xelatex" }
 g.vimtex_compiler_latexmk = {
-    aux_dir = ".build",
-    -- out_dir = "",
-    options = {
-	    "-shell-escape",
-        "-verbose",
-	    "-file-line-error",
-	    "-synctex=1",
-	    "-interaction=nonstopmode",
+	aux_dir = ".build",
+	-- out_dir = "",
+	options = {
+		"-shell-escape",
+		"-verbose",
+		"-file-line-error",
+		"-synctex=1",
+		"-interaction=nonstopmode",
 	},
 }
-
